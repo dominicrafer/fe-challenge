f6983e46e155876d157bd95ff079e6c21fd66a71
Christian Dominic Rafer <dominic.rafer@ecloudvalley.com>
2022-12-27 12:50:16 +0800

    app setup

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<span class="text-[99px]">testtest</span>
+  
+</template>
+
+<script>
+export default {
+  setup() {
+    console.log('TEST?')
+  }
+}
+</script>
+
+<style>
+
+</style>
