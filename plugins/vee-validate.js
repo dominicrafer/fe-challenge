45f247d7337aef3ba2870b01f1d449d2322b1c81
Dominic <dominic.rafer@ecloudvalley.com>
2024-02-08 10:52:46 +0800

    update: nuxt version fix: vee-validate plugin loading issue

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -2,56 +2,59 @@ import * as VeeValidate from "vee-validate";
 import * as AllRules from "@vee-validate/rules";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component('VForm', VeeValidate.Form)
-  nuxtApp.vueApp.component('VField', VeeValidate.Field)
-  nuxtApp.vueApp.component('VErrorMessage', VeeValidate.ErrorMessage)
-  const { $_ } = useNuxtApp()
-  Object.keys(AllRules).forEach(rule => {
-    VeeValidate.defineRule(rule, AllRules[rule]);
-  });
-  
-  VeeValidate.defineRule('phone_number', value => {
+  nuxtApp.vueApp.component("VForm", VeeValidate.Form);
+  nuxtApp.vueApp.component("VField", VeeValidate.Field);
+  nuxtApp.vueApp.component("VErrorMessage", VeeValidate.ErrorMessage);
+  const { $_ } = useNuxtApp();
+  $_.chain(AllRules)
+    .keys()
+    .value()
+    .forEach((rule) => {
+      VeeValidate.defineRule(rule, AllRules[rule]);
+    });
+
+  VeeValidate.defineRule("phone_number", (value) => {
     if (!/^(09|\+639)\d{9}$/.test(value)) {
-      return 'This field must be a valid phone_number';
+      return "This field must be a valid phone_number";
     }
     return true;
   });
 
-  VeeValidate.defineRule('has_number', value => {
+  VeeValidate.defineRule("has_number", (value) => {
     if (!/\d/.test(value)) {
-      return 'Must contain at least one number';
+      return "Must contain at least one number";
     }
     return true;
-  })
-  VeeValidate.defineRule('has_special_char', value => {
+  });
+  VeeValidate.defineRule("has_special_char", (value) => {
     if (!/[!@#$%^&*]/.test(value)) {
-      return 'Must contain at least one special character';
+      return "Must contain at least one special character";
     }
     return true;
-  })
-  VeeValidate.defineRule('has_upper_lower_case', value => {
+  });
+  VeeValidate.defineRule("has_upper_lower_case", (value) => {
     if (!/^(?=.*[a-z])(?=.*[A-Z])/.test(value)) {
-      return 'Must contain both uppercase and lowercase characters';
+      return "Must contain both uppercase and lowercase characters";
     }
     return true;
-  })
-  VeeValidate.defineRule('no_spaces', (value) => {
+  });
+  VeeValidate.defineRule("no_spaces", (value) => {
     if (/\s/g.test(value)) {
-      return 'This field cannot contain any spaces.';
+      return "This field cannot contain any spaces.";
     }
     return true;
   });
 
-  VeeValidate.defineRule('ph_phone_number', (value) => {
+  VeeValidate.defineRule("ph_phone_number", (value) => {
     if (!/^\+639\d{9}$/g.test(value)) {
-      return 'Invalid mobile number format';
+      return "Invalid mobile number format";
     }
     return true;
   });
 
-  VeeValidate.defineRule('select_required', (value) => {
+  VeeValidate.defineRule("select_required", (value) => {
     if ($_.isArray(value)) {
-      return !value.length ? false : true
+      return !value.length ? false : true;
     }
     if (!value) {
       return false;
@@ -59,10 +62,9 @@ export default defineNuxtPlugin((nuxtApp) => {
     return true;
   });
 
-  
-  VeeValidate.defineRule('confirm_password', (value, [otherValue]) => {
+  VeeValidate.defineRule("confirm_password", (value, [otherValue]) => {
     if (value !== otherValue) {
-      return 'Passwords do not match';
+      return "Passwords do not match";
     }
     return true;
   });
@@ -70,22 +72,23 @@ export default defineNuxtPlugin((nuxtApp) => {
   // Customize global validation rules error message
   VeeValidate.configure({
     generateMessage: (context) => {
-
       switch (context.rule.name) {
         case "email":
           return "Invalid email address";
         case "min":
           return `Must contain at least ${context.rule.params[0]} characters`;
         case "max":
-          return `${$_.startCase(context.field)} must only contain ${context.rule.params[0]} characters`;
+          return `${$_.startCase(context.field)} must only contain ${
+            context.rule.params[0]
+          } characters`;
         case "required":
-          return `Enter ${$_.startCase(context.field)}`
+          return `Enter ${$_.startCase(context.field)}`;
         case "digits":
-          return `${$_.startCase(context.field)} exceeds expected value`
+          return `${$_.startCase(context.field)} exceeds expected value`;
         case "phone_number":
-          return `Invalid mobile number format`
+          return `Invalid mobile number format`;
         case "select_required":
-          return `Select ${$_.startCase(context.field)}`
+          return `Select ${$_.startCase(context.field)}`;
         default:
           return `${$_.startCase(context.field)} is invalid`;
       }
